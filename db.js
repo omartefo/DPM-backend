4430a8d6431116a234c40f1889786eff83706daf
faridullah1 <faridullah996@gmail.com>
2024-09-02 12:18:36 +0500

    USER env variable named changed, it was conflicting with user variable on Godaddy

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
 console.log('env variables ...');
-console.log(process.env.DATABASE, process.env.USER, process.env.PASSWORD);
+console.log(process.env.DB_USER, process.env.USER, process.env.PASSWORD);
 
-const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const connection = new Sequelize(process.env.DB_USER, process.env.USER, process.env.PASSWORD, {
 	define: {
 		charset: 'utf8',
 		collate: 'utf8_general_ci', 
